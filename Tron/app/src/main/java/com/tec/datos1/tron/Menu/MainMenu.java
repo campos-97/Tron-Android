7abb0ac1dcee91fcb84f39605d4a8a6eb8751ad7
Roberto <roberm98@outlook.com>
2016-10-05 23:36:09 -0600

    Revert "Revert "Unificacion Interfaz Grafica""
    This reverts commit 9f5509f0875759b4208c728454c8b0ec6d4d0468.

---
+++ Tron/app/src/main/java/com/tec/datos1/tron/Menu/MainMenu.java
@@ -0,0 +1,51 @@
+package com.tec.datos1.tron.Menu;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.content.IntentCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.tec.datos1.tron.R;
+import com.tec.datos1.tron.gui.MainActivity;
+import static com.tec.datos1.tron.R.layout.activity_main_menu;
+
+public class MainMenu extends AppCompatActivity {
+
+    private Button button2;
+    private Button button1;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main_menu);
+
+        final MediaPlayer mediaPlayer;
+        mediaPlayer = MediaPlayer.create(this,R.raw.electro);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(100,100);
+        mediaPlayer.start();
+
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent button2 = new Intent(MainMenu.this, acerca.class);
+                mediaPlayer.stop();
+                startActivity(button2);
+
+            }
+        });
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent button1 = new Intent(MainMenu.this, MainActivity.class);
+                startActivity(button1);
+            }
+        });
+
+    }
+}
--- Tron/app/src/main/java/com/tec/datos1/tron/gui/MainActivity.java
+++ Tron/app/src/main/java/com/tec/datos1/tron/gui/MainActivity.java
@@ -1,31 +1,68 @@
 package com.tec.datos1.tron.gui;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
-
+import android.widget.TextView;
 import com.tec.datos1.tron.R;
-import com.tec.datos1.tron.client.ClientTask;
-
-/**
- * @author Andres Campos
- */
 
 public class MainActivity extends Activity {
+
+    public int velocidad = 5;
+    public int escudo = 5;
+    private Button button_escudo;
+    private Button button_velocidad;
+
+    TextView text_notificacion1;
+    TextView text_notificacion2;
+
     private GLSurfaceView glView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //glView = new MyGLSurfaceView(this);
-        //setContentView(glView);
-
-        setContentView(R.layout.main );
-        glView = new MyGLSurfaceView( this );
-        LinearLayout openGLLayout = (LinearLayout)findViewById(R.id.openGLLayout);
+        setContentView(R.layout.main);
+        glView = new MyGLSurfaceView(this);
+        LinearLayout openGLLayout = (LinearLayout) findViewById(R.id.openGLLayout);
         openGLLayout.addView(glView);
-    }
 
+
+        //CREACION DEL BOTON DEL ESCUDO
+        button_escudo = (Button) findViewById(R.id.button1);
+        button_escudo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (escudo>0) {
+                    escudo--;
+                    text_notificacion1 = (TextView) findViewById(R.id.badge1);
+                    text_notificacion1.setText(String.valueOf(escudo));
+                }else{
+                    text_notificacion1 = (TextView) findViewById(R.id.badge1);
+                    text_notificacion1.setText(String.valueOf(escudo));
+
+                }
+            }
+        });
+
+        //CREACION DEL BOTON VELOCIDAD
+        button_velocidad = (Button) findViewById(R.id.button2);
+        button_velocidad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (velocidad>0) {
+                    velocidad--;
+                    text_notificacion2 = (TextView) findViewById(R.id.badge2);
+                    text_notificacion2.setText(String.valueOf(velocidad));
+                }else{
+                    text_notificacion2 = (TextView) findViewById(R.id.badge2);
+                    text_notificacion2.setText(String.valueOf(velocidad));
+                }
+            }
+        });
+    }
 }
 
